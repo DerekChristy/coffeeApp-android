f4e6cc05dedb9951bdda6f48cb089b27251679a7
DerekChristy <kered.christy@gmail.com>
2020-01-28 23:19:59 +0530

    UI tweaked

--- app/src/main/java/com/example/coffeegrab/CartActivity.java
+++ app/src/main/java/com/example/coffeegrab/CartActivity.java
@@ -6,30 +6,36 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class CartActivity extends AppCompatActivity {
 
     ArrayList<Coffee> cartItems;
+    TextView priceTxt;
+    private float amount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
 
         Intent intent = getIntent();
-//        Bundle args = intent.getBundleExtra("bundle");
-//        cartItems = (ArrayList<Coffee>) args.getSerializable("list");
         cartItems = (ArrayList<Coffee>) intent.getSerializableExtra("list");
         ListView listView = findViewById(R.id.itemsList);
+        amount = 0;
+        for (Coffee item : cartItems) {
+            amount += Float.valueOf(item.getPrice()) * item.getQuantity();
+        }
+        priceTxt = findViewById(R.id.priceTxt);
+        priceTxt.setText(String.valueOf(amount));
         CartListAdapter adapter = new CartListAdapter(this, R.layout.cart_item, cartItems);
         listView.setAdapter(adapter);
-
     }
 
     public void prevActivity(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(intent, 0);
     }
 
     public void confirmOrder(View view) {
